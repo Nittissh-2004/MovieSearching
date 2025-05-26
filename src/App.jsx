13141eb2607d6ee219160d7a133352758a8033fb
Nittissh-2004 <nittissh2004@gmail.com>
2025-05-26 19:47:26 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import MovieDetails from './Pages/MovieDetails';
+import MovieDetails from './pages/Moviedetails';
 
 const App = () => {
   return (
